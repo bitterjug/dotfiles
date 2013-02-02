ed850064554df0b7bc2bc5f5f885a2dd4c80fd2e
Mark Skipper <marks@aptivate.org>
2013-02-02 22:23:24 +0000

    Add some bash stuff

---
+++ bash/gvimserver.sh
@@ -0,0 +1,19 @@
+set -o vi
+# Depends on xdotool being installed
+# alias vi='gvim --remote-tab-silent'
+vi(){
+    SERVER="MYGVIM"
+    if [ -n "${1:+x}" ] 
+    then
+        gvim --servername $SERVER --remote-tab-silent "$*"
+    else 
+        VIM=`xdotool search --name $SERVER`
+        if [ -n "${VIM:+x}" ]
+        then
+            gvim --servername $SERVER --remote-send :tabnew
+        else
+            gvim --servername $SERVER
+        fi
+    fi
+    xdotool search --name $SERVER windowactivate
+}
