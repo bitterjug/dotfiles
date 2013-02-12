0ee6bb87a900a1f2585094d4322017c0c701068c
Mark Skipper <marks@aptivate.org>
2013-02-12 21:06:03 +0000

    Make rotate script work

--- bin/rotate.sh
+++ bin/rotate.sh
@@ -1,4 +1,5 @@
 # Set rotation for screen and wacom
+PROGNAME=${0##*/}
 orientation="$1"
 
 function rotatewacom(){
@@ -14,6 +15,68 @@ function rotatescreen(){
     xrandr --orientation $1
 }
 
+function orient(){
+    stylus=`xsetwacom --list devices | head -1 | cut -d: -f2 | cut -f1`
+    export CURRENT=`xsetwacom --get $stylus Rotate`
+}
+
+function flip(){
+    orient
+    case "$CURRENT" in
+        none)
+            rotatescreen inverted 
+            rotatewacom half
+            ;;
+        *)
+            rotatescreen normal 
+            rotatewacom none
+            ;;
+    esac
+}
+
+function rotate(){
+    orient
+    case "$CURRENT" in
+        none)
+            rotatescreen right 
+            rotatewacom cw
+            ;;
+        cw)
+            rotatescreen inverted 
+            rotatewacom half
+            ;;
+        half)
+            rotatescreen left 
+            rotatewacom ccw
+            ;;
+        *)
+            rotatescreen normal 
+            rotatewacom none
+            ;;
+    esac
+}
+
+function use(){
+cat << EOF
+$PROGNAME [option]
+
+Where [option] is one of:
+
+left|inverted|right|normal
+
+    Set orientation
+
+flip
+
+    flip between normal and inverted
+
+rotate
+
+    rotate clockwise by a quarter
+
+EOF
+}
+
 case "$orientation" in 
     left)
         rotatescreen left 
@@ -27,9 +90,17 @@ case "$orientation" in
         rotatescreen right 
         rotatewacom cw
         ;;
-    *) 
+    normal) 
         rotatescreen normal 
         rotatewacom none
+        ;;
+    rotate)
+        rotate
+        ;;
+    flip)
+        flip
+        ;;
+    *)  use
         ;;
 esac
 
