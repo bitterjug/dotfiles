cb902a1550681fb6bcc11ab86492c3a524f9d610
Mark Skipper <marks@aptivate.org>
2013-02-15 22:34:26 +0000

    man colors use tput not hard coded

--- bash/colorman.sh
+++ bash/colorman.sh
@@ -1,8 +1,27 @@
 # Less Colors for Man Pages
-export LESS_TERMCAP_mb=$'\E[01;31m'       # begin blinking
-export LESS_TERMCAP_md=$'\E[01;38;5;74m'  # begin bold
-export LESS_TERMCAP_me=$'\E[0m'           # end mode
-export LESS_TERMCAP_se=$'\E[0m'           # end standout-mode
-export LESS_TERMCAP_so=$'\E[38;5;246m'    # begin standout-mode - info box
-export LESS_TERMCAP_ue=$'\E[0m'           # end underline
-export LESS_TERMCAP_us=$'\E[04;38;5;146m' # begin underline
+# Blnk
+export LESS_TERMCAP_mb=$(tput bold; tput setaf 6) 
+# Bold
+export LESS_TERMCAP_md=$(tput bold; tput setaf 184) 
+# End all -- normal
+export LESS_TERMCAP_me=$(tput sgr0)
+# Standout
+export LESS_TERMCAP_so=$(tput bold; tput setab 6; tput setaf 15)
+# End standout
+export LESS_TERMCAP_se=$(tput rmso; tput sgr0)
+# Start Underline
+export LESS_TERMCAP_us=$(tput smul; tput setaf 178)
+# End Underline
+export LESS_TERMCAP_ue=$(tput rmul; tput sgr0)
+# Reverse
+export LESS_TERMCAP_mr=$(tput rev)
+# Half Bright
+export LESS_TERMCAP_mh=$(tput dim)
+# Stat Subsecipt
+export LESS_TERMCAP_ZN=$(tput ssubm)
+# End Subsecipt
+export LESS_TERMCAP_ZV=$(tput rsubm)
+# Start Superscript
+export LESS_TERMCAP_ZO=$(tput ssupm)
+# End Superscript
+export LESS_TERMCAP_ZW=$(tput rsupm)
