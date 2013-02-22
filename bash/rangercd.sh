1263361476cb721ceeba980923ee1c992d6f8c4a
Mark Skipper <marks@aptivate.org>
2013-02-22 14:11:17 +0000

    Add rangercd bash function

---
+++ bash/rangercd.sh
@@ -0,0 +1,14 @@
+# Taken from ranger manual, use ^O to use Ranger for cd
+
+function ranger-cd {
+  tempfile='/tmp/chosendir'
+  /usr/bin/ranger --choosedir="$tempfile" "${@:-$(pwd)}"
+  test -f "$tempfile" &&
+  if [ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]; then
+    cd -- "$(cat "$tempfile")"
+  fi
+  rm -f -- "$tempfile"
+}
+
+# This binds Ctrl-O to ranger-cd:
+bind '"\C-o":"ranger-cd\C-m"'
