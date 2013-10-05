455db40bbbc83a11d3fd36a5653ded5ca785e378
Mark Skipper <marks@aptivate.org>
2013-10-05 23:02:09 +0100

    Xmonad: add to fresh, prev and next WS
    Add the freshrc needed to get this into the right place
    otherwise I Cant compile it.
    
    Basic prev and next WS work, bound to F and G

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,9 +1,15 @@
 import XMonad
 import XMonad.Config.Gnome
+import XMonad.Actions.CycleWS
 import qualified Data.Map as M
 
 keysToAdd x = 
-    [ ]
+    [ 
+        -- Previous workspace
+            ((modMask x, xK_f), prevWS)
+        -- Next workspace
+       ,    ((modMask x, xK_g), nextWS)
+    ]
 
 
 myKeys x = M.union (keys gnomeConfig x) (M.fromList (keysToAdd x))
