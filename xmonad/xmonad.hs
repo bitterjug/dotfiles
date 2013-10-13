ca0451fab49e5d33f6b2b5b252f9649bed9e8489
Mark Skipper <marks@aptivate.org>
2013-10-13 22:36:16 +0100

    xmonad: add fake maximize binding

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -1,7 +1,19 @@
 import XMonad
+import XMonad.StackSet
+import XMonad.Util.WorkspaceCompare
 import XMonad.Config.Gnome
 import XMonad.Actions.CycleWS
 import qualified Data.Map as M
+
+-- maximize: move window to next empty ws, and follow it
+maximize :: X()
+maximize = doTo Next EmptyWS getSortByIndex shiftAndFollow
+
+-- Shift active window to selected workspace, and view it there.
+shiftAndFollow :: WorkspaceId -> X ()
+shiftAndFollow i = do
+    (windows . shift) i
+    (windows . view) i
 
 keysToAdd x = 
     [ 
@@ -9,9 +21,18 @@ keysToAdd x =
            (((modMask x .|. controlMask), xK_h), prevWS)
         -- Next workspace
        ,   (((modMask x .|. controlMask), xK_l), nextWS)
+        -- 'minimize' to first empty workspace
+       ,   ((modMask x, xK_i), shiftTo Next EmptyWS)
+        -- 'maximize' to first empty workspace, and view
+       ,   (((modMask x .|. controlMask), xK_i), maximize)
+        -- move currnet window next and follow it
+        ,   ((modMask x, xK_plus), shiftToNext >> nextWS)
+        -- move currnet window previous and follow it
+       ,   ((modMask x, xK_minus), shiftToPrev >> prevWS)
         -- launch synapse
        ,   ((modMask x, xK_o), spawn "synapse")
     ]
+
 
 myManagementHooks :: [ManageHook]
 myManagementHooks = 
