7ba46266f8ac818945570049b1ddd612b31fa4d6
Mark Skipper <marks@aptivate.org>
2014-01-19 17:12:16 +0000

    Change dev journal file extensions to markdown

--- bin/note.sh
+++ bin/note.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-NOTEFILE=$HOME/SparkleShare/devjournal/devjournal.md
+NOTEFILE=$HOME/SparkleShare/devjournal/devjournal.markdown
 COMMAND="call append(line('$'), ['', join(['##', strftime('%F %R %A') ]),'',''])"
 gvim -c"$COMMAND" "$NOTEFILE" -c"$" 
-
