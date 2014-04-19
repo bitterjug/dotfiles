75282e01a3990f10e816fc0935e91bec13dc8cb9
Mark Skipper <marks@aptivate.org>
2014-04-19 22:20:56 +0100

    Switch to new version of devjoyrnal script

--- bin/note.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-NOTEFILE=$HOME/SparkleShare/devjournal/devjournal.markdown
-COMMAND="call append(line('$'), ['', join(['##', strftime('%F %R %A') ]),'',''])"
-gvim -c"$COMMAND" "$NOTEFILE" -c"$" 
