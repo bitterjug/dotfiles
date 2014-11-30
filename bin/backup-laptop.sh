b5cdea07a18944494fead59e2a3f192246dff2cc
Mark Skipper <marks@aptivate.org>
2014-11-30 21:43:23 +0000

    backup: adjust for multiple backup devices

--- bin/backup-laptop.sh
+++ bin/backup-laptop.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
 FROM=/home/$USER 
-TO=/media/$USER/backup
+DEST_MOUNT=/media/$USER/backup-[0-9]
 
-echo -e "\n\nBackup from $FROM to $TO \n"
+echo -e "\nBackup $USER's home directory to USB backup disk\n"
+echo -e "Testing for mounted backup devices...\n"
 
-echo -e "Checking destination drive is mounted..."
-if ! mount | grep $TO 
-then
-    echo "Make sure $TO is mounted!"   
-    echo "Did you plug in the USB Drive?"
-    exit 1
-fi
+# count mounted media matchin DEST_MOUNT pattern
+mounted=`mount | grep $DEST_MOUNT  | wc -l`
 
+# We're expecting exactly one
+
+case "$mounted" in
+    0)  echo "No mounted device found at $DEST_MOUNT"
+        echo "Make sure the USB drive is connected."
+        exit 1
+        ;;
+    1)  TO=`ls -d  $DEST_MOUNT`
+        echo "Found backup drive: $TO"
+        ;;
+    *)  echo "Found $mounted drives; run again  with only one connected."
+        exit 1
+        ;;
+esac
+
+echo -e "Backup from $FROM to $TO \n"
 
 # Don't back up:
 # - things that give errors (eg sockets)
