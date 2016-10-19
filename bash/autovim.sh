efdf3aa20f228fba495c7f97ee1fbc9539c4ada1
Mark Skipper <marks@aptivate.org>
2016-10-19 18:48:38 +0100

    Add vi and gitv aliases

--- bash/autovim.sh
+++ bash/autovim.sh
@@ -2,3 +2,6 @@
 
 bind '"\C-n":" NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim\C-m"'
 
+# Use neo vim as vi
+alias vi='NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim'
+
---
+++ bash/gitv.sh
@@ -0,0 +1,2 @@
+# gitv as alias for vim's  
+alias gitv='NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim "+ set cmdheight=2" +Gitv .'
