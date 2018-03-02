206201678d273d89638890df097c70fc085a813c
Mark Skipper <mark.skipper@featurespace.co.uk>
2018-03-02 10:39:08 +0000

    Colour std err in git pause

--- bash/gitpause.sh
+++ bash/gitpause.sh
@@ -3,11 +3,11 @@ GIT=$(which git)
 function git {
   if [ "$1" == "rebase" ] || [ "$1" == "checkout" ] || [ "$1" == "merge" ] || [ "$1" == "reset" ] || [ "$1" == "pull" ] || [ "$1" == "revert" ] || [ "$1" == "stash" ]
   then
-    echo "pausing watch ..." >&2
+    echo -e "\e[1;31mPausing watch ... \e[0m" >&2
     pkill -TSTP -f watch.js
     $GIT $*
     local ret=$?
-    echo "resuming watch ..." >&2
+    echo -e "\e[1;31mResuming watch ... \e[0m" >&2
     pkill -CONT -f watch.js
     return $ret
   else
