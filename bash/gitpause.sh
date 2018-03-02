ad5c38776af9a4f386717d6b01d2530c6452cbe7
Mark Skipper <mark.skipper@featurespace.co.uk>
2018-03-02 09:02:04 +0000

    Add more commands to pause git

--- bash/gitpause.sh
+++ bash/gitpause.sh
@@ -1,7 +1,7 @@
 # Pause watch.js during git command
 GIT=$(which git)
 function git {
-  if [ "$1" == "rebase" ] || [ "$1" == "checkout" ] || [ "$1" == "merge" ] || [ "$1" == "reset" ]
+  if [ "$1" == "rebase" ] || [ "$1" == "checkout" ] || [ "$1" == "merge" ] || [ "$1" == "reset" ] || [ "$1" == "pull" ] || [ "$1" == "revert" ] || [ "$1" == "stash" ]
   then
     echo "pausing watch ..." >&2
     pkill -TSTP -f watch.js
