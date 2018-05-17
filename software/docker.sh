fa1229ec202a3bdcd90a1ea6f333386e8bff9f6f
Mark <bitterjug@gmail.com>
2018-05-17 14:52:30 +0100

    add notes on installing docker

---
+++ software/docker.sh
@@ -0,0 +1,3 @@
+# Install dependencies for docker
+# we're following: https://docs.docker.com/install/linux/docker-ce/ubuntu/#install-using-the-repository
+sudo apt install apt-transport-https ca-certificates curl software-properties-common
