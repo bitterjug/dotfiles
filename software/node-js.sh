e4983db5bfab3ba308d7d0fe5b51dee0a537a1b2
Mark <bitterjug@gmail.com>
2018-06-05 07:39:00 +0100

    Software, ranger and devicons
    Devicons to take advanage of new Elm icons

--- software/node-js.sh
+++ software/node-js.sh
@@ -1,4 +1,5 @@
-#we're installing via NVM
-# https://github.com/creationix/nvm#installation
+# install NVM https://github.com/creationix/nvm#installation
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
 
+# install node
+nvm install node
