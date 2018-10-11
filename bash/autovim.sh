2877961643929c99b28d9b79c1f1f3e63380c457
Mark <bitterjug@gmail.com>
2018-10-11 17:57:40 +0100

    Start vim with ^p

--- bash/autovim.sh
+++ bash/autovim.sh
@@ -1,6 +1,10 @@
 # Open vim from bash on control key
 
-bind '"\C-n":" NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim\C-m"'
+# Control n opens nerdtree
+bind '"\C-n":" nvim -c NERDTree \C-m"'
+
+# Control p opens fzf
+bind '"\C-p":" nvim -c ProjectMru \C-m"'
 
 # Use neo vim as vi
 alias vi='NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim'
