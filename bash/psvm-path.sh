b0cd88beee8e047ee51cd6c51e28260f11af0d05
Mark <bitterjug@gmail.com>
2018-11-10 18:30:51 +0000

    purescript path for psvm

---
+++ bash/psvm-path.sh
@@ -0,0 +1,7 @@
+# Add ~/.psvm/current/bin to the path
+# this is where https://github.com/ThomasCrevoisier/psvm-js
+# puts the currently active version of pursescript if you
+#      npm install -g psvm
+#      psvm install $(psvm latest)
+#      psvm use $(psvm latest)
+PATH=~/.psvm/current/bin:$PATH
