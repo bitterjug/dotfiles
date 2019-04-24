1a9b9c4212a2ed06ee33d422a7d3bcdd42aa63fd
Mark <mark@featurespace.co.uk>
2019-04-24 13:00:58 +0100

    stuff to make new laptop work

--- software/ranger.sh
+++ software/ranger.sh
@@ -8,7 +8,7 @@ then
     git pull
 else
     git clone git@github.com:ranger/ranger.git
-    cd ctags
+    cd ranger
 fi
 
 sudo python setup.py install --prefix=$HOME/.local --optimize=1 --record=install_log.txt
