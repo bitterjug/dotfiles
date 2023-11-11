e17b634ebe8f64304150e8289a9977360e6b23f1
Mark <bitterjug@gmail.com>
2023-11-11 17:21:24 +0000

    chad: conform formatter for lua and elm

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -3,71 +3,130 @@ local overrides = require("custom.configs.overrides")
 ---@type NvPluginSpec[]
 local plugins = {
 
-  -- Override plugin definition options
+	-- Override plugin definition options
 
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-      -- format & linting
-      {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-          require "custom.configs.null-ls"
-        end,
-      },
-    },
-    config = function()
-      require "plugins.configs.lspconfig"
-      require "custom.configs.lspconfig"
-    end, -- Override to setup mason-lspconfig
-  },
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = {
+			-- format & linting
+			{
+				"jose-elias-alvarez/null-ls.nvim",
+				config = function()
+					require("custom.configs.null-ls")
+				end,
+			},
+		},
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.configs.lspconfig")
+		end, -- Override to setup mason-lspconfig
+	},
 
-  -- override plugin configs
-  {
-    "williamboman/mason.nvim",
-    opts = overrides.mason
-  },
+	-- override plugin configs
+	{
+		"williamboman/mason.nvim",
+		opts = overrides.mason,
+	},
 
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = overrides.treesitter,
-  },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = overrides.treesitter,
+	},
 
-  {
-    "nvim-tree/nvim-tree.lua",
-    opts = overrides.nvimtree,
-  },
+	{
+		"nvim-tree/nvim-tree.lua",
+		opts = overrides.nvimtree,
+	},
 
-  -- Install a plugin
-  {
-    "max397574/better-escape.nvim",
-    event = "InsertEnter",
-    config = function()
-      require("better_escape").setup()
-    end,
-  },
+	-- Install a plugin
+	{
+		"max397574/better-escape.nvim",
+		event = "InsertEnter",
+		config = function()
+			require("better_escape").setup()
+		end,
+	},
 
+	{
+		"karb94/neoscroll.nvim",
+		event = { "BufReadPre", "BufNewFile" },
 
-  {
-    "karb94/neoscroll.nvim",
-    lazy = false,
-    config = function ()
-      require('neoscroll').setup {}
-    end
-  },
-  -- To make a plugin not be loaded
-  -- {
-  --   "NvChad/nvim-colorizer.lua",
-  --   enabled = false
-  -- },
+		config = function()
+			require("neoscroll").setup({})
+		end,
+	},
 
-  -- All NvChad plugins are lazy-loaded by default
-  -- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
-  -- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
-  -- {
-  --   "mg979/vim-visual-multi",
-  --   lazy = false,
-  -- }
+	{
+		"Mr-LLLLL/interestingwords.nvim",
+		config = function()
+			require("interestingwords").setup({
+				colors = {
+					"#0a7383",
+					"#a07040",
+					"#4070a0",
+					"#40a070",
+					"#70a040",
+					"#0070e0",
+					"#007020",
+					"#d4a00d",
+					"#06287e",
+					"#5b3674",
+					"#4c8f2f",
+					"#1060a0",
+					"#a0b0c0",
+					"#aeee00",
+					"#ff0000",
+					"#0000ff",
+					"#b88823",
+					"#ffa724",
+					"#ff2c4b",
+				},
+				search_count = true,
+				navigation = true,
+				search_key = "<leader>ms",
+				cancel_search_key = "<leader>mM",
+				color_key = "<leader>mm",
+				cancel_color_key = "<leader>mn",
+			})
+		end,
+		keys = {
+			"<leader>ms",
+			"<leader>mM",
+			"<leader>mm",
+			"<leader>mn",
+		},
+	},
+
+	{
+		"stevearc/conform.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					elm = { "elm-format" },
+				},
+				format_on_save = {
+					-- These options will be passed to conform.format()
+					timeout_ms = 500,
+					lsp_fallback = true,
+				},
+			})
+		end,
+	},
+	-- To make a plugin not be loaded
+	-- {
+	--   "NvChad/nvim-colorizer.lua",
+	--   enabled = false
+	-- },
+
+	-- All NvChad plugins are lazy-loaded by default
+	-- For a plugin to be loaded, you will need to set either `ft`, `cmd`, `keys`, `event`, or set `lazy = false`
+	-- If you want a plugin to load on startup, add `lazy = false` to a plugin spec, for example
+	-- {
+	--   "mg979/vim-visual-multi",
+	--   lazy = false,
+	-- }
 }
 
 return plugins
