4e8f944cb0053601e3ecfe3423dff341bc729017
Mark <bitterjug@gmail.com>
2023-11-17 21:58:26 +0000

    chad: Force change root on tree toggle

--- nvim/lua/custom/mappings.lua
+++ nvim/lua/custom/mappings.lua
@@ -4,7 +4,7 @@ local M = {}
 M.general = {
 	n = {
 		[";"] = { ":", "enter command mode", opts = { nowait = true } },
-		["-"] = { "<cmd> NvimTreeFindFileToggle<CR>", "Find file in tree view" },
+		["-"] = { "<cmd> NvimTreeFindFileToggle!<CR>", "Find file in tree view" },
 	},
 	v = {
 		[">"] = { ">gv", "indent" },
