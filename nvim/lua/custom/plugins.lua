00841d48b2e411018f6f82684342f5f524de00f9
Mark <bitterjug@gmail.com>
2024-03-12 20:23:20 +0000

    Add obsidian (and diagnostic window)

--- nvim/lua/custom/plugins.lua
+++ nvim/lua/custom/plugins.lua
@@ -184,6 +184,12 @@ local plugins = {
 			require("rust-tools").setup({})
 		end,
 	},
+	{
+		"cseickel/diagnostic-window.nvim",
+		dependencies = { "MunifTanjim/nui.nvim" },
+		ft = "elm",
+		cmd = { "DiagWindowShow" },
+	},
 	-- To make a plugin not be loaded
 	-- {
 	--   "NvChad/nvim-colorizer.lua",
@@ -198,6 +204,57 @@ local plugins = {
 	--   "mg979/vim-visual-multi",
 	--   lazy = false,
 	-- }
+	{
+		"epwalsh/obsidian.nvim",
+		version = "*", -- recommended, use latest release instead of latest commit
+		cmd = {
+			"ObsidianOpen",
+			"ObsidianQuickSwitch",
+			"ObsidianNew",
+			"ObsidianSearch",
+			"ObsidianTemplate",
+			"ObsidianToday",
+			"ObsidianTomorrow",
+			"ObsidianYesterday",
+			"ObsidianDailies",
+			"ObsidianTags",
+		},
+		lazy = true,
+		ft = "markdown",
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+		--   "BufReadPre path/to/my-vault/**.md",
+		--   "BufNewFile path/to/my-vault/**.md",
+		-- },
+		dependencies = {
+			-- Required.
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+			"nvim-telescope/telescope.nvim",
+		},
+		opts = {
+			workspaces = {
+				{
+					name = "default",
+					path = "~/Documents/vim-notes",
+				},
+			},
+
+			-- see below for full list of options 👇
+			daily_notes = {
+				-- Optional, if you keep daily notes in a separate directory.
+				folder = "diary",
+				-- Optional, if you want to change the date format for the ID of daily notes.
+				-- date_format = "%Y-%m-%d",
+				-- Optional, if you want to change the date format of the default alias of daily notes.
+				-- alias_format = "%B %-d, %Y",
+				-- Optional, if you want to automatically insert a template from your template directory like 'daily.md'
+				-- template = nil,
+			},
+		},
+	},
 }
 
 return plugins
